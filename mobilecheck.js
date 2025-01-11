04e79aad5d6087c344fe01f12dc33f0d4f6e9bb5
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-01-11 18:30:40 +0000

    Add mobile site

--- mobilecheck.js
+++ mobilecheck.js
@@ -2,8 +2,12 @@ function isMobileDevice() {
     return /iPhone|iPad|iPod|Android|BlackBerry|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent);
 }
 
+function shouldForceDesktop() {
+    return window.location.hash === "#forcedesktop";
+}
+
 window.onload = function() {
-    if (isMobileDevice()) {
+    if (isMobileDevice() && !shouldForceDesktop()) {
         window.location.href = "/mobile";
     }
 };

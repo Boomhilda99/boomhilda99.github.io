3797bb3917caccf588e8c2647f75deb4692ebc29
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-01-12 02:11:19 +0000

    Add portfolio placeholder

---
+++ portfolio/mobilecheck-portfolio.js
@@ -0,0 +1,13 @@
+function isMobileDevice() {
+    return /iPhone|iPad|iPod|Android|BlackBerry|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent);
+}
+
+function shouldForceDesktop() {
+    return window.location.hash === "#forcedesktop";
+}
+
+window.onload = function() {
+    if (isMobileDevice() && !shouldForceDesktop()) {
+        window.location.href = "/mobile/portfolio";
+    }
+};

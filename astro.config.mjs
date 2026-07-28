88807dfa562bb2a2b523691cd7cdc1944735f850
Boomhilda99 <oliverdonald04172011@gmail.com>
2026-07-28 20:17:56 +0100

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+
+export default defineConfig({
+  integrations: [icon()],
+  site: "https://boomhilda99.github.io",
+});
